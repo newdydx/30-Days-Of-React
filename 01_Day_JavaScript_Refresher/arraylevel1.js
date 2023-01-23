654c43fe50c6d1d8f8910aafc7805f837fe19528
heritage <heritageololade@gmail.com>
2023-01-23 17:42:30 +0100

    day1

---
+++ 01_Day_JavaScript_Refresher/arraylevel1.js
@@ -0,0 +1,23 @@
+ export const emptyArray = []
+const array = [5,'heritage',34,'ololade',78]
+console.log(array.length)
+console.log(array[0],array[2],array[array.length -1])
+const mixedDataTypes = [5, "heritage", { name: herit }, "ololade", 78];
+const itCompanies = ['facebook','google','microsoft','amazon','oracle','apple','ibm']
+console.log(itCompanies)
+console.log(itCompanies.length)
+console.log(itCompanies[0],itCompanies[3],itCompanies[6])
+console.log(itCompanies)
+console.log(itCompanies.toUpperCase());
+console.log(itCompanies.split(''))
+// 8888
+// 4444
+console.log(itCompanies.sort())
+console.log(itCompanies.reverse())
+itCompanies.slice(0,4)
+itCompanies.slice(4,7)
+itCompanies.slice(3,4)
+itCompanies.shift()
+// 222
+itCompanies.pop()
+itCompanies.slice(0)
---
+++ 01_Day_JavaScript_Refresher/arraylevel2.js
@@ -0,0 +1,28 @@
+// exercice number 2
+let text = "I love teaching and empowering people I teach HTML CSS JS React Python";
+const array = text.split(" ");
+console.log(array.length);
+// exercice number 3
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+shoppingCart.unshift('meat')
+shoppingCart.push('sugar')
+shoppingCart[2] = "greentea"
+// exercise number 4
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+if (countries.includes('Ethopia')) {
+    console.log("Ethopia")
+}else {
+    countries.push("Ethopia")
+}
